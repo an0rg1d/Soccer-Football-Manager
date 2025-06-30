aebdf01e09f9e7051d752b6861b4a7458abd2639
football/soccer <muhammmaedovmasrur@gmail.com>
2025-06-30 12:53:23 -0600

    Created the fixture with players

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'football')]
-    public function homepage(
-
-    ): Response
+    public function index(PlayerRepository $playerRepository):Response
     {
-        return $this->render('main/homepage.html.twig');
+        // Fetch the players from the database
+        $players = $playerRepository->findAll();
+
+        return $this->render('main/homepage.html.twig', [
+            'players' => $players])
+            ;
+
     }
 }
