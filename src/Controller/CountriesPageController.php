804aebdbba9436d566c5d8e3e724b57b0795f151
Masrur Mukhammedov <muhammmaedovmasrur@gmail.com>
2026-01-02 14:32:57 -0700

    new updates in countries page, now it appears on main screen, but still doesn't work.

--- src/Controller/CountriesPageController.php
+++ src/Controller/CountriesPageController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\CountriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\CountriesRepository;
 
 class CountriesPageController extends AbstractController
 {
-    #[Route('/countries', name: 'main')]
-    public function countriespage(CountriesRepository $countriesRepository):Response
+    #[Route('/countries', name: 'countries')]
+    public function countriespage(CountriesRepository $countriesRepository): Response
     {
-        $countries = $countriesRepository->findAllOrderedByName();
+        $countries = $countriesRepository->findAll();
 
         return $this->render('main/countries.html.twig', [
             'countries' => $countries
