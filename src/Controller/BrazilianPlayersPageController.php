7c7c26790eb8983f502162bb156ee427059a3fb0
Masrur Mukhammedov <muhammmaedovmasrur@gmail.com>
2026-01-05 15:28:06 -0700

    Added unfinished Brazilian players page controller

---
+++ src/Controller/BrazilianPlayersPageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BrazilianPlayersPageController extends AbstractController
+{
+    #Route("/brazilian-players", name="brazilian_players")
+
+}
