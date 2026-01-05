ce38f75d8a82c41ffd570cd217724d83d826e5fc
Masrur Mukhammedov <muhammmaedovmasrur@gmail.com>
2026-01-05 15:29:58 -0700

    Added unfinished Brazilian biggest achievements in sport page controller

---
+++ src/Controller/BrazilianAchievementsPageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BrazilianAchievementsPageController extends AbstractController
+{
+    #Route("/brazilian-achievements", name="brazilian_achievements")
+
+}
